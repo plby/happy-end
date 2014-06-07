ce847a6e207c52ab4de2f121f28ca88505faaef4
plby <balexeev@princeton.edu>
2014-06-07 15:53:19 -0700

    Framework with a little test case.

--- src/sat.pl
+++ src/sat.pl
@@ -10,11 +10,46 @@ $N //= 6;
 $K //= 2**($N-2) + 1;
 
 # Initialize variables
-my( $VARS, %var, %name );
+my( $VARS, @CLAUSES, %var, %name );
 $VARS = 0;
+var( "x" );
+var( "y" );
+var( "z" );
 
 # Generate SAT instance
+clause( var("x"), var("y"), var("z") );
+clause( sat_not(var("x")), sat_not(var("y")), sat_not(var("z")) );
 
+# Interact with SAT solver
+use IPC::Open3;
+my( $solver, $pid );
+$solver = "/usr/bin/picosat";
+$pid = open3( \*SAT_IN, \*SAT_OUT, "", $solver );
+print SAT_IN "p cnf $VARS ", 0+@CLAUSES, "\n";
+for my $clause ( @CLAUSES ) {
+	print SAT_IN join " ", @$clause, "0\n";
+}
+close( SAT_IN );
+
+my( $response, $result );
+$response = <SAT_OUT>;
+if( $response =~ m/s unsatisfiable/i ) {
+	print "Problem found unsatisfiable as desired.\n";
+} elsif( $response =~ m/s satisfiable/i ) {
+	my( @assignment );
+	@assignment = ("") x $VARS;
+	while( $response = <SAT_OUT> ) {
+		while( $response =~ m/ (\d+)\b/g ) {
+			$assignment[$1] = 1;
+		}
+	}
+	print "Satisfiable assignment:\n";
+	for my $var ( 1 .. $VARS ) {
+		printf "%6d  %1s  %-20s\n", $var, ($assignment[$var] ? "1" : "0"), $name{$var};
+	}
+} else {
+	print "Could not understand SAT solver response:\n$response";
+}
 
 # Subroutines
 sub var {
@@ -25,4 +60,14 @@ sub var {
 		$name{$VARS} = $name;
 	}
 	return $var{$name};
+}
+
+sub sat_not {
+	my( $var ) = @_;
+	return -$var;
+}
+
+sub clause {
+	my( @vars ) = @_;
+	push @CLAUSES, [@vars];
 }
